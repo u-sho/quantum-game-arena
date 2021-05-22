2afd9658b802c96cc09d9fe45ef1428e13d33a30
u-sho (Shouhei Uechi) <26079835+u-sho@users.noreply.github.com>
2021-05-23 04:35:34 +0900

    PWA settings (#24)
    * add service-worker
    * add mock favicon
    * add webmanifest
    * config svelte/kit

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		amp: false,
+		// TODO: floc: null,
+		ssr: false,
+		target: '#svelte',
+		trailingSlash: 'never'
 	}
 };
 
