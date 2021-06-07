ce934204beb6606a04d309e981acfcd3241f9aa6
u-sho <shouhei.uechi@gmail.com>
2021-06-07 14:18:24 +0900

    add test that assign other types

--- src/lib/types/test/generics.test.ts
+++ src/lib/types/test/generics.test.ts
@@ -6,33 +6,79 @@ type BaseOptions = {
 	baz: string;
 };
 
-test('RequiredAtLeastOne<T>', () => {
-	// at least one of 'foo', 'bar', 'baz' is required
+describe('RequiredAtLeastOne<T>', () => {
+	// One of 'foo', 'bar', 'baz' is required at least.
 	type Options = RequiredAtLeastOne<BaseOptions>;
 
-	// @ts-expect-error
-	const opt0: Options = {};
-	const opt11: Options = { foo: 3 };
-	const opt12: Options = { bar: 3 };
-	const opt13: Options = { baz: 's' };
-	const opt21: Options = { bar: 5, baz: 'foo' };
-	const opt22: Options = { foo: 5, baz: 'foo' };
-	const opt23: Options = { foo: 5, bar: 2 };
-	const opt3: Options = { foo: 123, bar: 123, baz: 'foo' };
+	test('is required at least one?', () => {
+		// @ts-expect-error
+		const opt0: Options = {};
+		const opt11: Options = { foo: 3 };
+		const opt12: Options = { bar: 3 };
+		const opt13: Options = { baz: 's' };
+		const opt21: Options = { bar: 5, baz: 'foo' };
+		const opt22: Options = { foo: 5, baz: 'foo' };
+		const opt23: Options = { foo: 5, bar: 2 };
+		const opt3: Options = { foo: 123, bar: 123, baz: 'foo' };
+	});
+
+	test('is assignable other types?', () => {
+		// @ts-expect-error
+		let opt: Options = { foo: 'str' };
+		// @ts-expect-error
+		opt = { bar: 'str' };
+		// @ts-expect-error
+		opt = { bar: 5, baz: 5 };
+		// @ts-expect-error
+		opt = { bar: 'str', baz: 'str' };
+		// @ts-expect-error
+		opt = { foo: 5, baz: 5 };
+		// @ts-expect-error
+		opt = { foo: 'str', baz: 'str' };
+		// @ts-expect-error
+		opt = { foo: 5, bar: 'str' };
+		// @ts-expect-error
+		opt = { foo: 'str', bar: 3 };
+		// @ts-expect-error
+		opt = { foo: 1, bar: 1, baz: 'ff', who: 4 };
+	});
 });
 
-test('RequiredAtLeastOne<T, K>', () => {
-	// at least one of 'foo', 'bar' is required
+describe('RequiredAtLeastOne<T, K>', () => {
+	// One of 'foo', 'bar' is required at least.
 	type NumOptions = RequiredAtLeastOne<BaseOptions, 'foo' | 'bar'>;
 
-	// @ts-expect-error
-	const npt0: NumOptions = {};
-	const npt11: NumOptions = { foo: 3 };
-	const npt12: NumOptions = { bar: 3 };
-	// @ts-expect-error
-	const npt13: NumOptions = { baz: 's' };
-	const npt21: NumOptions = { bar: 5, baz: 'foo' };
-	const npt22: NumOptions = { foo: 5, baz: 'foo' };
-	const npt23: NumOptions = { foo: 5, bar: 2 };
-	const npt3: NumOptions = { foo: 123, bar: 123, baz: 'foo' };
+	test('is required at least one of "foo"|"bar" ?', () => {
+		// @ts-expect-error
+		const npt0: NumOptions = {};
+		const npt11: NumOptions = { foo: 3 };
+		const npt12: NumOptions = { bar: 3 };
+		// @ts-expect-error
+		const npt13: NumOptions = { baz: 's' };
+		const npt21: NumOptions = { bar: 5, baz: 'foo' };
+		const npt22: NumOptions = { foo: 5, baz: 'foo' };
+		const npt23: NumOptions = { foo: 5, bar: 2 };
+		const npt3: NumOptions = { foo: 123, bar: 123, baz: 'foo' };
+	});
+
+	test('is assignable other types?', () => {
+		// @ts-expect-error
+		let npt: NumOptions = { foo: 'str' };
+		// @ts-expect-error
+		npt = { bar: 'str' };
+		// @ts-expect-error
+		npt = { bar: 5, baz: 5 };
+		// @ts-expect-error
+		npt = { bar: 'str', baz: 'str' };
+		// @ts-expect-error
+		npt = { foo: 5, baz: 5 };
+		// @ts-expect-error
+		npt = { foo: 'str', baz: 'str' };
+		// @ts-expect-error
+		npt = { foo: 5, bar: 'str' };
+		// @ts-expect-error
+		npt = { foo: 'str', bar: 3 };
+		// @ts-expect-error
+		npt = { foo: 1, bar: 1, baz: 'ff', who: 4 };
+	});
 });
