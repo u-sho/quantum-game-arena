35c5aac06b01ce7ed2e82176615d68e827108962
u-sho <shouhei.uechi@gmail.com>
2021-06-10 22:44:02 +0900

    refactor

--- src/lib/utility/getNumeral.ts
+++ src/lib/utility/getNumeral.ts
@@ -1,5 +1,19 @@
-// refactor: template literal type return annotation
-function getOrdinal(n: number): string {
+type OrdinalNumeral =
+	| 'first'
+	| 'second'
+	| 'third'
+	| 'fourth'
+	| 'fifth'
+	| 'sixth'
+	| 'seventh'
+	| 'eighth'
+	| 'ninth'
+	| `${number}1st`
+	| `${number}2nd`
+	| `${number}3rd`
+	| `${number}th`;
+
+function getOrdinal(n: number): OrdinalNumeral {
 	if (n === 1) return 'first';
 	if (n === 2) return 'second';
 	if (n === 3) return 'third';
@@ -9,10 +23,10 @@ function getOrdinal(n: number): string {
 	if (n === 7) return 'seventh';
 	if (n === 8) return 'eighth';
 	if (n === 9) return 'ninth';
-	if (n % 10 === 1) return `${n}st`;
-	if (n % 10 === 2) return `${n}nd`;
-	if (n % 10 === 3) return `${n}rd`;
-	return `${n}th`;
+	if (n % 10 === 1) return `${n}st` as `${number}1st`;
+	if (n % 10 === 2) return `${n}nd` as `${number}2nd`;
+	if (n % 10 === 3) return `${n}rd` as `${number}3rd`;
+	return `${n}th` as `${number}th`;
 }
 
 export { getOrdinal };
