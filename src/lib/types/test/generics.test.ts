d40dd01fb2092a791e85387d4fead3e723b469dd
u-sho (Shouhei Uechi) <26079835+u-sho@users.noreply.github.com>
2022-04-24 19:29:52 +0900

    upgrade TypeScript and its compiler options (#78)
    * upgrade TypeScript and its compiler options
    
    - update TypeScript from 4.5 to 4.6
    - disallow `.js`
    - use `tslib`
    - add DOM to lib
    - add typescript-styled-plugin
    - add typescript-eslint-language-service

--- src/lib/types/test/generics.test.ts
+++ src/lib/types/test/generics.test.ts
@@ -12,14 +12,14 @@ describe('RequiredAtLeastOne<T>', () => {
 
 	test('is required at least one?', () => {
 		// @ts-expect-error
-		const opt0: Options = {};
-		const opt11: Options = { foo: 3 };
-		const opt12: Options = { bar: 3 };
-		const opt13: Options = { baz: 's' };
-		const opt21: Options = { bar: 5, baz: 'foo' };
-		const opt22: Options = { foo: 5, baz: 'foo' };
-		const opt23: Options = { foo: 5, bar: 2 };
-		const opt3: Options = { foo: 123, bar: 123, baz: 'foo' };
+		let opt: Options = {};
+		opt = { foo: 3 };
+		opt = { bar: 3 };
+		opt = { baz: 's' };
+		opt = { bar: 5, baz: 'foo' };
+		opt = { foo: 5, baz: 'foo' };
+		opt = { foo: 5, bar: 2 };
+		opt = { foo: 123, bar: 123, baz: 'foo' };
 	});
 
 	test('is assignable other types?', () => {
@@ -50,35 +50,35 @@ describe('RequiredAtLeastOne<T, K>', () => {
 
 	test('is required at least one of "foo"|"bar" ?', () => {
 		// @ts-expect-error
-		const npt0: NumOptions = {};
-		const npt11: NumOptions = { foo: 3 };
-		const npt12: NumOptions = { bar: 3 };
+		let numOpt: NumOptions = {};
+		numOpt = { foo: 3 };
+		numOpt = { bar: 3 };
 		// @ts-expect-error
-		const npt13: NumOptions = { baz: 's' };
-		const npt21: NumOptions = { bar: 5, baz: 'foo' };
-		const npt22: NumOptions = { foo: 5, baz: 'foo' };
-		const npt23: NumOptions = { foo: 5, bar: 2 };
-		const npt3: NumOptions = { foo: 123, bar: 123, baz: 'foo' };
+		numOpt = { baz: 's' };
+		numOpt = { bar: 5, baz: 'foo' };
+		numOpt = { foo: 5, baz: 'foo' };
+		numOpt = { foo: 5, bar: 2 };
+		numOpt = { foo: 123, bar: 123, baz: 'foo' };
 	});
 
 	test('is assignable other types?', () => {
 		// @ts-expect-error
-		let npt: NumOptions = { foo: 'str' };
+		let numOpt: NumOptions = { foo: 'str' };
 		// @ts-expect-error
-		npt = { bar: 'str' };
+		numOpt = { bar: 'str' };
 		// @ts-expect-error
-		npt = { bar: 5, baz: 5 };
+		numOpt = { bar: 5, baz: 5 };
 		// @ts-expect-error
-		npt = { bar: 'str', baz: 'str' };
+		numOpt = { bar: 'str', baz: 'str' };
 		// @ts-expect-error
-		npt = { foo: 5, baz: 5 };
+		numOpt = { foo: 5, baz: 5 };
 		// @ts-expect-error
-		npt = { foo: 'str', baz: 'str' };
+		numOpt = { foo: 'str', baz: 'str' };
 		// @ts-expect-error
-		npt = { foo: 5, bar: 'str' };
+		numOpt = { foo: 5, bar: 'str' };
 		// @ts-expect-error
-		npt = { foo: 'str', bar: 3 };
+		numOpt = { foo: 'str', bar: 3 };
 		// @ts-expect-error
-		npt = { foo: 1, bar: 1, baz: 'ff', who: 4 };
+		numOpt = { foo: 1, bar: 1, baz: 'ff', who: 4 };
 	});
 });
