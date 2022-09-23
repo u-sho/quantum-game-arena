8f743db0b01d0e6945cd995fef41b7dff59d7612
u-sho <26079835+u-sho@users.noreply.github.com>
2022-09-23 21:32:29 +0900

    upgrade SvelteKit
    from 1.0.0-next.363 to 1.0.0-next.364

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		port: 3000,
+		strictPort: true
+	},
+	server: {
+		port: 3000,
+		strictPort: true
+	}
 };
 
 export default config;
