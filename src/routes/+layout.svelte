a9a9c2a7dcf2d4ec00b293663f8251e260cf0f2c
u-sho <shouhei.uechi@gmail.com>
2023-06-02 04:29:15 +0900

    get starting vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 import TheHeader from '$lib/TheHeader/index.svelte';
 import '../global.css';
+
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <TheHeader />
