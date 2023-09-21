83ffd36f368b68b7b9be07c7b6cbb864937aa4b9
u-sho (Shouhei Uechi) <26079835+u-sho@users.noreply.github.com>
2023-09-21 14:44:24 +0900

    upgrade `@typescript-eslint` to v6 (#134)
    * upgrade typescript-eslint to v6
    
    - remove duplicated rules
    - remove default options of rule
    - fetch new default rules
    
    * add `@typescript-eslint/explicit-function-return-type` rule
    
    * add @typescript-eslint/strict-type-checked rules
    
    * add some `@typescript-eslint` rules
    - @typescript-eslint/explicit-member-accessibility
    - @typescript-eslint/naming-convention
    - @typescript-eslint/no-unnecessary-qualifier
    - @typescript-eslint/no-useless-empty-export
    - @typescript-eslint/prefer-enum-initializers
    - @typescript-eslint/prefer-readonly
    
    * refactor: parameters should be readonly
    
    * add some `@typescript-eslint` rules
    - `@typescript-eslint/prefer-regexp-exec` for performance
    - `@typescript-eslint/promise-function-async`
    - `@typescript-eslint/require-array-sort-compare`
    - `@typescript-eslint/switch-exhaustiveness-check`
    
    * update eslint and typescript-eslint to the latest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,8 +18,9 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@typescript-eslint/strict-type-checked',
 		'prettier'
 	],
 	plugins: ['svelte3', '@typescript-eslint'],
@@ -33,13 +34,23 @@ module.exports = {
 			}
 		},
 		{
+			files: ['*.js', '*.cjs'],
+			rules: { '@typescript-eslint/explicit-function-return-type': 'off' }
+		},
+		{
 			files: ['*.cjs'],
 			rules: { '@typescript-eslint/no-require-imports': 'off' }
+		},
+		{
+			files: ['./*.config.*', '.eslintrc.cjs'],
+			rules: { '@typescript-eslint/naming-convention': 'off' }
 		}
 	],
 	rules: {
 		'no-console': isProduction() ? 'error' : 'off',
+
 		eqeqeq: ['error', 'always', { null: 'ignore' }],
+		'no-duplicate-imports': ['error', { includeExports: true }],
 		'no-unused-expressions': 'error',
 		'no-var': 'error',
 		'prefer-const': 'error',
@@ -49,17 +60,75 @@ module.exports = {
 
 		'@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
 		'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-		'@typescript-eslint/consistent-type-imports': [
-			'error',
-			{ prefer: 'type-imports', disallowTypeAnnotations: true }
+		'@typescript-eslint/consistent-type-exports': 'error',
+		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/explicit-member-accessibility': ['warn', { accessibility: 'no-public' }],
+		'@typescript-eslint/member-delimiter-style': 'warn',
+		'@typescript-eslint/method-signature-style': 'error',
+		camelcase: 'off',
+		'@typescript-eslint/naming-convention': [
+			'warn',
+			{
+				selector: 'default',
+				format: ['camelCase'],
+				leadingUnderscore: 'forbid',
+				trailingUnderscore: 'forbid'
+			},
+			{
+				selector: 'variable',
+				modifiers: ['global', 'const'],
+				format: ['camelCase', 'UPPER_CASE']
+			},
+			{
+				selector: 'memberLike',
+				modifiers: ['private'],
+				format: ['camelCase'],
+				leadingUnderscore: 'require'
+			},
+			{
+				selector: 'memberLike',
+				modifiers: ['protected'],
+				format: ['camelCase'],
+				leadingUnderscore: 'require'
+			},
+			{
+				selector: 'typeLike',
+				format: ['PascalCase']
+			},
+			{
+				// for non-exported functions
+				selector: 'function',
+				modifiers: ['global'],
+				format: ['camelCase'],
+				leadingUnderscore: 'require'
+			},
+			{
+				selector: 'function',
+				modifiers: ['exported', 'global'],
+				format: ['camelCase'],
+				leadingUnderscore: 'forbid'
+			},
+			{
+				// exception for QuantumTTT player names
+				selector: 'objectLiteralProperty',
+				filter: { regex: '^(X|Y)$', match: true },
+				format: ['PascalCase']
+			}
 		],
-		'@typescript-eslint/member-delimiter-style': 'error',
-		'@typescript-eslint/method-signature-style': ['error', 'property'],
-		'@typescript-eslint/no-confusing-non-null-assertion': 'warn',
-		'@typescript-eslint/no-duplicate-imports': 'error',
-		'@typescript-eslint/no-implicit-any-catch': 'error',
 		'@typescript-eslint/no-import-type-side-effects': 'error',
 		'@typescript-eslint/no-require-imports': 'error',
+		'@typescript-eslint/no-unnecessary-qualifier': 'error',
+		'@typescript-eslint/no-useless-empty-export': 'error',
+		'@typescript-eslint/prefer-enum-initializers': 'error',
+		'@typescript-eslint/prefer-readonly': 'error',
+		// '@typescript-eslint/prefer-readonly-parameter-types': 'error',
+		'@typescript-eslint/prefer-regexp-exec': 'error',
+		'@typescript-eslint/promise-function-async': 'error',
+		'@typescript-eslint/require-array-sort-compare': 'error',
+		'@typescript-eslint/switch-exhaustiveness-check': 'error',
+
+		'@typescript-eslint/non-nullable-type-assertion-style': 'off',
 		'@typescript-eslint/unbound-method': 'off'
 	},
 	settings: {
