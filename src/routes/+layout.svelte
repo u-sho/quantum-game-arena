a37eb29825513de6e617613a5e3ffa4e2bb99920
u-sho <26079835+u-sho@users.noreply.github.com>
2024-02-27 23:46:40 +0900

    add `@vercel/speed-insights`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@ import './global.css';
 
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 inject({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights();
 </script>
 
 <AppHeader />
