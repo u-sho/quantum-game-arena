2a16ecc316f546a73813a7fbbb17c20f90745b63
u-sho <26079835+u-sho@users.noreply.github.com>
2025-08-11 18:46:27 +0900

    use `performance.now` instead of `Date.now`

--- src/lib/utils/sleep.ts
+++ src/lib/utils/sleep.ts
@@ -8,9 +8,9 @@ if (import.meta.vitest) {
 	const { test, expect } = import.meta.vitest;
 	test('sleep', async () => {
 		const sleepTime = 100;
-		const start = Date.now();
+		const start = performance.now();
 		await sleep(sleepTime);
-		const end = Date.now();
+		const end = performance.now();
 		expect(end - start).toBeGreaterThanOrEqual(sleepTime);
 	});
 }
